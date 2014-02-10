67a3eded54ece126609a0cfbcb5106b558b8accf
gryphius <oli@fuglu.org>
2014-02-10 15:39:26 +0100

    don't remember

--- test/accountconfig.py
+++ test/accountconfig.py
@@ -3,3 +3,9 @@ SERVER2_HOST="192.168.23.62"
 USERNAME="user1"
 PASSWORD="pass1"
 SSL=False
+
+
+SERVER1_HOST="fumailsynctest1.cmtest.ch"
+SERVER2_HOST="fumailsynctest2.cmtest.ch"
+USERNAME="catchall@nichtantworten.ch"
+PASSWORD="sp4m"
--- test/test_mass_append_server1.py
+++ test/test_mass_append_server1.py
@@ -3,7 +3,7 @@
 
 """Test: Append messages to box 1 and check if they are synced"""
 
-NUMMESSAGES=100
+NUMMESSAGES=3000
 MAILBOX="INBOX"
 CHECK_INTERVAL=5
 MAX_WAIT_SYNC=30
@@ -15,17 +15,34 @@ import time
 import sys
 
 imap1=get_imap_connection(SERVER1_HOST, USERNAME, PASSWORD,assertEmpty=True)
+imap2=get_imap_connection(SERVER2_HOST, USERNAME, PASSWORD,assertEmpty=True)
 
 print "appending messages..."
 
 #Append messages to server1
 counter=0
+failcount=0
 for message in create_dummy_txt_messages(NUMMESSAGES):
-    counter+=1
     content=message.as_string()
-    imap1.append(MAILBOX,None,None,content)
+    #mix flags
+    flags=""
+    if counter%2==0:
+      flags+="\\Seen "
+    if counter%3==0:
+      flags+="\\Deleted "
+    result=imap1.append(MAILBOX,flags,None,content)
+    if result[0]!='OK':
+      print "Warning, did not get OK result!"
+      failcount+=1
+    else:
+      counter+=1
+      
     if counter%10==0:
-        print "%s/%s"%(counter,NUMMESSAGES)
+	r,server1count=imap1.select(MAILBOX)
+	server1count=int(server1count[0])
+	r,server2count=imap2.select(MAILBOX)
+	server2count=int(server2count[0])
+        print "%s of %s uploaded, %s failed, server1: %s server2: %s"%(counter,NUMMESSAGES,failcount,server1count,server2count)
         
 
 #Check Message count on server 1
@@ -33,11 +50,10 @@ print "Verifying amount on server 1..."
 time.sleep(2)
 result,messagecount=imap1.select(MAILBOX)
 messagecount=int(messagecount[0])
-assert messagecount==NUMMESSAGES,"Server1: Expected %s messages, got %s"%(NUMMESSAGES,messagecount)
+assert messagecount==counter,"Server1: Expected %s messages, got %s"%(counter,messagecount)
 
 
 #check sync to server 2
-imap2=get_imap_connection(SERVER2_HOST, USERNAME, PASSWORD,assertEmpty=False)
 now=time.time()
 timeout=now+MAX_WAIT_SYNC
 
@@ -48,7 +64,7 @@ while True:
     messagecount=int(messagecount[0])
     print "Current messagecount on server 2: %s"%messagecount
     
-    if messagecount==NUMMESSAGES:
+    if messagecount==counter:
         print "Mailboxes are in sync!"
         break
     
